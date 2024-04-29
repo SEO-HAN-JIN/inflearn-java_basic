e9b3b0b5cc48ddc53ef61bb3b0251b4fc5242ef7
unknown <seo970713@gmail.com>
2024-04-30 02:27:14 +0900

    패키지

---
+++ src/main/java/pack/PackageMain1.java
@@ -0,0 +1,11 @@
+package pack;
+
+import pack.Data;
+import pack.a.User;
+
+public class PackageMain1 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+    }
+}
