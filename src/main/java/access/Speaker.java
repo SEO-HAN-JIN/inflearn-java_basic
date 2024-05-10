04536c03124813bf0ded12454178bd3dc70977f8
unknown <seo970713@gmail.com>
2024-05-11 01:05:30 +0900

    접근제어자

--- src/main/java/access/Speaker.java
+++ src/main/java/access/Speaker.java
@@ -1,7 +1,19 @@
 package access;
 
 public class Speaker {
-    int volume;
+
+
+    /*
+        Speaker 객체를 사용하는 사용자는 Speaker 의 volume 필드와 메서드에 모두 접근할 수 있다.
+        앞서 volumeUp()과 같은 메서드를 만들어서 음량이 100을 넘지 못하도록 기능을 개발했지만 소용없다. 왜냐하면 Speaker 를 사용하는 입장에서는
+        volume 필드에 직접 접근해서 값을 설정할 수 있기 때문이다.
+     */
+//    int volume;
+
+    /*
+        private 접근자는 모든 외부 호출을 막는다. 따라서 private이 붙은 경우 해당 클래스 내부에서만 호출할 수 있다.
+     */
+    private int volume;
 
     Speaker(int volume) {
         this.volume = volume;
--- src/main/java/access/SpeakerMain.java
+++ src/main/java/access/SpeakerMain.java
@@ -1,6 +1,7 @@
 package access;
 
 public class SpeakerMain {
+
     public static void main(String[] args) {
         Speaker speaker = new Speaker(90);
         speaker.showVolume();
@@ -11,9 +12,10 @@ public class SpeakerMain {
         speaker.volumeUp();
         speaker.showVolume();
 
-        // 필드에 직접 접근
+        // 필드에 직접 접근 - 문제
         System.out.println("volume 필드 직접 접근 수정");
-        speaker.volume = 200;
+//        speaker.volume = 200;
         speaker.showVolume();
     }
+
 }
