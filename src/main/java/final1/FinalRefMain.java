3a07a394359b490be7504dccb231c28e78164253
unknown <seo970713@gmail.com>
2024-05-28 01:05:42 +0900

    extend

--- src/main/java/final1/FinalRefMain.java
+++ src/main/java/final1/FinalRefMain.java
@@ -2,7 +2,8 @@ package final1;
 
 public class FinalRefMain {
     public static void main(String[] args) {
-        final Data data = new Data(); // 참조값을 더 이상 바꿀 수 없음
+        final Data data = new Data();
+//        data = new Data();    // 에러!! 참조값을 더 이상 바꿀 수 없음
 
         // 참조 대상의 값은 변경 가능
         data.value = 10;
